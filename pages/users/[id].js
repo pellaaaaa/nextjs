c4f65d22fc2cde10c7afd5c27922a00149ae40b1
pellaaaaa <fellaattaqi16@gmail.com>
2024-02-01 15:47:57 +0700

    tugas hari-8

---
+++ pages/users/[id].js
@@ -0,0 +1,14 @@
+import Layout from "@/layout";
+import { useRouter } from "next/router";
+
+export default function UsersByName(){
+    const router = useRouter();
+    const { id } = router?.query;
+
+    return (
+        <Layout>
+            <p>Users By Name {id}</p>
+        </Layout>
+    );
+
+}

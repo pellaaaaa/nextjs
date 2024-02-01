89d3cfce0f3ba6b9ea4118a544d43638814a9479
pellaaaaa <fellaattaqi16@gmail.com>
2024-02-01 17:43:22 +0700

    tugas hari-9

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import Layout from "@/layout";
+import dynamic from "next/dynamic";
+import { useEffect } from "react";
+
+const LayoutComponent = dynamic(() => import ("@/layout"));
 
 export default function Main() {
+    useEffect(() => {
+        fetch("/api/hello")
+        .then((res) => res.json())
+        .then((res) => console.log("response =>", res))
+        .catch((err) => console.log("err =>", err));
+    }, []);
+
  return (
   <div>
-   <Layout metaTitle="Home">
+   <LayoutComponent metaTitle="Home">
     <p>Home</p>
-   </Layout>
+   </LayoutComponent>
   </div>
  );
 }
